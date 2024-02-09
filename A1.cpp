6a296fcc275d1d040de3e00b0578d7251b9f87ef
minhquan38 <khuongvuminhquan38@gmail.com>
2024-02-09 15:11:05 -0500

    Kinda working but not yet

--- A1.cpp
+++ A1.cpp
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ReliableUDP.h"
+#include "hash-library/md5.h"
+#include "hash-library/md5.cpp"
 
 #pragma warning(disable : 4996)
 
 const char* kPacketDelimiter = "|";
 const int kNumericBase = 10;
+
 
 void displayHelp()
 {
@@ -70,4 +73,52 @@ static void unpackData(char *packet, char *fileName, int packetTotal, int packet
             }
         }
     }
+}
+void packData(unsigned char packet[kPacketSize], char fileName[kFileNameSize], short packetTotal, short packetOrder, unsigned char fileContent[kFileContentSize])
+{
+    memcpy(packet + kUdpHeaderSize, fileName, kFileNameSize);
+    memcpy(packet + kUdpHeaderSize + kFileNameSize, &packetTotal, kPacketTotalSize);
+    memcpy(packet + kUdpHeaderSize + kFileNameSize + kPacketTotalSize, &packetOrder, kPacketOrderSize);
+    memcpy(packet + kUdpHeaderSize + kFileNameSize + kPacketTotalSize + kPacketOrderSize, fileContent, kFileContentSize);
+    char checksum[kChecksumSize + 1];
+    generateChecksum(checksum, packet);
+    checksum[kChecksumSize] = '\0';
+    printf("Checksum: %s", checksum);
+    memcpy(packet + kUdpHeaderSize + kFileNameSize + kPacketTotalSize + kPacketOrderSize + kFileContentSize, checksum, kChecksumSize);
+}
+
+void unpackData(unsigned char packet[kPacketSize], char fileName[kFileNameSize + 1], unsigned short* packetTotal, unsigned short* packetOrder, unsigned char fileContent[kFileContentSize], char checksum[kChecksumSize + 1])
+{
+    memcpy(fileName, packet + kUdpHeaderSize, kFileNameSize);
+    fileName[kFileNameSize] = '\0';
+    *packetTotal = packet[kUdpHeaderSize + kFileNameSize];
+    *packetTotal += packet[kUdpHeaderSize + kFileNameSize + 1] << 8;
+    *packetOrder = packet[kUdpHeaderSize + kFileNameSize + kPacketTotalSize];
+    *packetOrder += packet[kUdpHeaderSize + kFileNameSize + kPacketTotalSize + 1] << 8;
+    memcpy(fileContent, packet + kUdpHeaderSize + kFileNameSize + kPacketTotalSize + kPacketOrderSize, kFileContentSize);
+    memcpy(checksum, packet + kUdpHeaderSize + kFileNameSize + kPacketTotalSize + kPacketOrderSize + kFileContentSize, kChecksumSize);
+    checksum[kChecksumSize] = '\0';
+}
+
+void generateChecksum(char checksum[kChecksumSize], unsigned char packet[kPacketSize])
+{
+    MD5 md5;
+    md5.add(packet, kPacketSize - kChecksumSize);
+    memcpy(checksum, md5.getHash().c_str(), kChecksumSize);
+}
+
+int compareChecksum(char checksum[kChecksumSize], unsigned char packet[kPacketSize])
+{
+    char generatedChecksum[kChecksumSize];
+    generateChecksum(generatedChecksum, packet);
+
+    for (int i = 0; i < kChecksumSize; i++)
+    {
+        if (checksum[i] != generatedChecksum[i])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
 }
--- ReliableUDP.h
+++ ReliableUDP.h
@@ -4,9 +4,18 @@
 
 const int kErrorNum = -1;
 const char kInvalidFilenameChars[] = { '\\', '/', ':', '*', '?', '"', '<', '>', '|' };
-const int kFilenameMaxLength = 255;
-const int kPacketSize = 500;
-const int kFileMaxSize = 31 * 1024 * 1024; // 31MB
+const int kUdpHeaderSize = 12;
+const int kFileNameSize = 50;
+const int kPacketTotalSize = sizeof(short);
+const int kPacketOrderSize = sizeof(short);
+const int kChecksumSize = 32;
+const int kFileContentSize = 384 - kFileNameSize - kPacketTotalSize - kPacketOrderSize - kChecksumSize;
+const int kPacketSize = kUdpHeaderSize + kFileNameSize + kPacketTotalSize + kPacketOrderSize + kFileContentSize + kChecksumSize;
+
 void displayHelp();
 static char* packData(char* fileName, short packetTotal, short packetOrder, char* fileContent);
 static void unpackData(char* packet, char* fileName, int packetTotal, int packetOrder, char* fileContent);
+void packData(unsigned char packet[kPacketSize], char fileName[kFileNameSize], short packetTotal, short packetOrder, unsigned char fileContent[kFileContentSize]);
+void unpackData(unsigned char packet[kPacketSize], char fileName[kFileNameSize + 1], unsigned short* packetTotal, unsigned short* packetOrder, unsigned char fileContent[kFileContentSize], char checksum[kChecksumSize + 1]);
+void generateChecksum(char checksum[kChecksumSize], unsigned char packet[kPacketSize]);
+int compareChecksum(char checksum[kChecksumSize], unsigned char packet[kPacketSize]);
